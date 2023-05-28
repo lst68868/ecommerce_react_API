3057d03fedb464e8450e3181babe1044ff784fe4
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-05-28 18:57:14 -0400

    heroku??

--- DB/Models/Products.js
+++ DB/Models/Products.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema({
+  id: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
