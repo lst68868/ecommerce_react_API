a4a200521e61ff37b08e9250a74272e62af1f139
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-05-28 19:07:17 -0400

    removed from schema

--- DB/Models/Products.js
+++ DB/Models/Products.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema({
-  id: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
